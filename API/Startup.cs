15532df202871b7985c9b9f9b5aed08a5d5f7ff0
ItsAnass <domanee@hotmail.com>
2022-02-21 14:09:13 +0100

    Created API paging, filtering, sorting and searching

--- API/Startup.cs
+++ API/Startup.cs
@@ -38,8 +38,15 @@ namespace API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
 
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
 
+                });
+            });
 
         }
 
@@ -60,6 +67,8 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
             app.UseSwaggerDocumentation();
