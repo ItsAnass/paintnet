4e4a6e925d9a8e7a22d45c6b094f14a4c745a977
ItsAnass <domanee@hotmail.com>
2022-02-22 15:55:09 +0100

    Some page design and created first api get

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'PaintNet';
+  products: any[];
+
+  constructor(private http: HttpClient ){}
+
+  ngOnInit(): void {
+   this.http.get('http://localhost:44333/api/Products').subscribe((response: any) => {
+
+     this.products = response.data;
+   }, error => {
+     console.log(error)   
+    });
+  
+  }
 }
