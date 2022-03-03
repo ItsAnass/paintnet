f60a4db0d1aba27aff49ff0db29833fa8518cae8
ItsAnass <domanee@hotmail.com>
2022-03-03 13:34:30 +0100

    Fixed the sorting and filtering

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IBrand } from '../shared/models/brand';
 import { IProduct } from '../shared/models/product';
+import { IType } from '../shared/models/productType';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -10,12 +12,30 @@ import { ShopService } from './shop.service';
 export class ShopComponent implements OnInit {
 
   products: IProduct[];
+  brands: IBrand[];
+  types: IType[];
+  brandIdSelected = 0;
+  typeIdSelected = 0;
+  sortSelected = 'name';
+  sortOptions=[
+    {name: 'Alphabetical',value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: Hight to Low', value: 'priceDesc'}
+  ];
 
   constructor(private shopService: ShopService) { }
 
   ngOnInit(){
 
-    this.shopService.getProducts().subscribe(response =>{
+    this.getProducts();
+    this.getBrands();
+    this.getTypes();
+
+   
+  }
+
+  getProducts(){
+    this.shopService.getProducts(this.brandIdSelected, this.typeIdSelected, this.sortSelected).subscribe(response =>{
       
       this.products = response.data
     
@@ -23,6 +43,41 @@ export class ShopComponent implements OnInit {
       
       console.log(error)
     })
+  }
+
+  getBrands(){
+    this.shopService.getBrands().subscribe(response =>{
+      this.brands = [{id: 0, name: 'All'}, ...response];
+
+    }, error =>{
+      
+      console.log(error)
+    })
+  }
+
+  getTypes(){
+    this.shopService.getTypes().subscribe(response =>{
+      this.types= [{id: 0, name: 'All'}, ...response];
+
+    }, error =>{
+      
+      console.log(error)
+    })
+  }
+
+  onBrandSelected(brandId:number){
+    this.brandIdSelected = brandId;
+    this.getProducts();
+  }
+
+  onTypeSelected(typeId:number){
+    this.typeIdSelected = typeId;
+    this.getProducts();
+  }
+
+  onSortSelected(sort: string){
+    this.sortSelected = sort;
+    this.getProducts();
   }
 
 }
